9a897f841f36a894ce68c0e51f3f25583822d41b
Marc Khouzam <marc.khouzam@gmail.com>
2019-08-03 23:00:49 -0400

    Block helm v2
    It is currently important not to run with helm v2 or else the
    'helm init' command will try to setup tiller on whatever cluster the
    user is currently pointing.
    
    So for now, we don't allow helm v2.
    
    Signed-off-by: Marc Khouzam <marc.khouzam@gmail.com>

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -38,6 +38,28 @@ fi
 export PATH="${VENV_DIR}/bin:${PATH}"
 export HELM_HOME="${ROBOT_OUTPUT_DIR}/.helm"
 rm -rf ${HELM_HOME} && mkdir -p ${HELM_HOME}
+
+# We only support helm v3 at this time.
+# To figure out which version of helm is used, we run 'helm version'
+# with the -c flag which is only supported in helm v2; if we get an
+# error, it means we are running helm v3, if we don't get an error,
+# it's helm v2 and we abort. We want to use the -c flag because if
+# we end up on helm v2 and we don't have that flag, it will try to
+# contact the cluster, which may not be accessible, and the command
+# will timeout.
+#
+# It is important not to run with helm v2 or else the 'helm init'
+# command will try to setup tiller on whatever cluster the user
+# is currently pointing to since we haven't setup the kind cluster yet.
+set +x
+if helm version -c > /dev/null; then
+    echo "Helm v2 not supported yet!"
+    echo "Please set the ROBOT_HELM_PATH environment variable" \
+         "to the directory where the helm v3 to test can be found."
+    exit 1
+fi
+set -x
+
 helm init
 if [[ ! -d ${ROBOT_VENV_DIR} ]]; then
     virtualenv -p $(which python3) ${ROBOT_VENV_DIR}
